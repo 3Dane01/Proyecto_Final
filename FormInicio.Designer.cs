0773356939fe37ad75d280af2b92720bb9197040
oscar_rodriguez <oscar@LAPTOP-OIL740UC>
2024-05-23 10:08:01 -0600

    bomba

--- FormInicio.Designer.cs
+++ FormInicio.Designer.cs
@@ -455,7 +455,7 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(579, 90);
+            this.pictureBox4.Location = new System.Drawing.Point(566, 90);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(202, 181);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
